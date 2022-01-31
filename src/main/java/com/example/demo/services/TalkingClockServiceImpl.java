b7cdb113e28c618993a891adcd47a77c65d984b6
SmrutiPani <41058308+SmrutiPani@users.noreply.github.com>
2022-01-31 14:46:12 +0530

    Update TalkingClockServiceImpl.java

--- src/main/java/com/example/demo/services/TalkingClockServiceImpl.java
+++ src/main/java/com/example/demo/services/TalkingClockServiceImpl.java
@@ -42,47 +42,35 @@ public class TalkingClockServiceImpl implements TalkingClockService {
 			if (min == 0) {
 				logger.debug("Time Entered - " + hr + " : " + min);
 				logger.debug(words[hr] + " O' clock");
-				//System.out.println("Time Entered - " + hr + " : " + min);
-				//System.out.println(words[hr] + " O' clock");
+				
 				timetext = words[hr] + " O' clock";
 			} else if (min == 15) {
 				logger.debug("Time Entered - " + hr + " : " + min);
 				logger.debug("Quarter Past " + words[hr]);
-				//System.out.println("Time Entered - " + hr + " : " + min);
-				//System.out.println("Quarter Past " + words[hr]);
 				timetext = "Quarter Past " + words[hr];
 
 			} else if (min == 30) {
 				logger.debug("Time Entered - " + hr + " : " + min);
 				logger.debug("Half Past " + words[hr]);
-				//System.out.println("Time Entered - " + hr + " : " + min);
-				//System.out.println("Half Past " + words[hr]);
 				timetext = "Half Past " + words[hr];
 			} else if (min == 45) {
 				logger.debug("Time Entered - " + hr + " : " + min);
 				logger.debug("Quarter to " + w);
-				//System.out.println("Time Entered - " + hr + " : " + min);
-				//System.out.println("Quarter to " + w);
 				timetext = "Quarter to " + w;
 			} else if (min < 30) // Condition for minutes between 1-29
 			{
 				logger.debug("Time Entered - " + hr + " : " + min);
 				logger.debug(words[min] + " past " + words[hr]);
-				//System.out.println("Time Entered - " + hr + " : " + min);
-				//System.out.println(words[min] + " past " + words[hr]);
 				timetext = words[min] + " past " + words[hr];
 			} else // Condition for minutes between 30-59
 			{
 				logger.debug("Time Entered - " + hr + " : " + min);
 				logger.debug(words[60 - min] + " to " + w);
-				//System.out.println("Time Entered - " + hr + " : " + min);
-				//System.out.println(words[60 - min] + " to " + w);
 				timetext = words[60 - min] + " to " + w;
 			}
 		} // end of outer if statement
 		else {
 			logger.debug("Invalid Time Entered");
-			//System.out.println("Invalid Time Entered");
 			timetext = "Invalid Time Entered";
 		}
 		tt.setResponse(timetext);
